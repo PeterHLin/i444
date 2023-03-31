f7f6b8590aefba79bd92df6c703b3dd0e20310a1
PeterHLin <plin11@binghamton.edu>
2023-03-31 17:25:21 -0400

    finished

--- submit/prj3-sol/src/lib/grades-ws.ts
+++ submit/prj3-sol/src/lib/grades-ws.ts
@@ -12,13 +12,13 @@ import { GradesDao } from 'cs544-prj2-sol';
 
 import { SelfLink, SuccessEnvelope, ErrorEnvelope }
   from './response-envelopes.js';
+import { ReadableStreamDefaultController } from 'stream/web';
 
 export type App = Express.Application;
 
 
 export default function serve(model: GradesDao, base = '/grades')
-  : App
-{
+  : App {
   const app = Express();
   app.locals.model = model;
   app.locals.base = base;
@@ -29,11 +29,14 @@ export default function serve(model: GradesDao, base = '/grades')
 
 function setupRoutes(app: Express.Application) {
   const base = app.locals.base;
-  app.use(cors({exposedHeaders: 'Location'}));
+  app.use(cors({ exposedHeaders: 'Location' }));
   app.use(Express.json());
 
   //TODO: add routes
-
+  app.get(`${base}/:COURSE_ID`, get_course_grades(app));
+  app.get(`${base}/:COURSE_ID/:ROW_ID`, get_course_grade_row(app));
+  app.post(`${base}/:COURSE_ID`, load_courses(app));
+  app.patch(`${base}/:COURSE_ID`, patch_courses(app));
   //must be last
   app.use(do404(app));
   app.use(doErrors(app));
@@ -42,10 +45,32 @@ function setupRoutes(app: Express.Application) {
 
 // A typical handler can be produced by running a function like
 // the following:
-function doSomeHandler(app: Express.Application) {
-  return async function(req: Express.Request, res: Express.Response) {
+function get_course_grades(app: Express.Application) {
+  return async function (req: Express.Request, res: Express.Response) {
     try {
       //code which uses req and res:
+      const course_id = req.params.COURSE_ID;
+      const table = req.query.full;
+      const result = (await app.locals.model.getGrades(course_id));
+      //check result status
+      if (result.isOk === true) {
+        res.location(course_id);
+      }
+      else {
+        throw result;
+      }
+      //check table query
+      if (table === "true") {
+        //if true get the full table
+        const response = selfResult<G.FullTable>(req, result.val.getFullTable());
+        res.json(response);
+      }
+      else
+      //else get the raw table
+      {
+        const response = selfResult<G.FullTable>(req, result.val.getRawTable());
+        res.json(response);
+      }
       //  req.query.X will provide value of query-parameter X.
       //  req.params.X will provide value of route parameter X.
       //  req.body will provide request body
@@ -55,7 +80,7 @@ function doSomeHandler(app: Express.Application) {
       //  res.json() to send back a JSON response, res.send() to
       //  send back a non-JSON response.
     }
-    catch(err) {
+    catch (err) {
       const mapped = mapResultErrors(err);
       res.status(mapped.status).json(mapped);
     }
@@ -63,18 +88,142 @@ function doSomeHandler(app: Express.Application) {
 }
 
 
+function get_course_grade_row(app: Express.Application) {
+  return async function (req: Express.Request, res: Express.Response) {
+    try {
+      //code which uses req and res:
+      const course_id = req.params.COURSE_ID;
+      const row = req.params.ROW_ID;
+      const table = req.query.full;
+      const result = (await app.locals.model.getGrades(course_id));
+      //check result status
+      if (result.isOk === true) {
+        res.location(course_id);
+      }
+      else {
+        throw result;
+      }
+      //check table query
+      if (table === "true") {
+        //if true get the full table
+        const response = selfResult<G.FullTable>(req, result.val.getFullTableRow(row));
+        res.json(response.result);
+      }
+      else
+      //else get the raw table
+      {
+        const response = selfResult<G.FullTable>(req, result.val.getRawTableRow(row));
+        res.json(response.result);
+      }
+      //  req.query.X will provide value of query-parameter X.
+      //  req.params.X will provide value of route parameter X.
+      //  req.body will provide request body
+      //  If a Result is in error simply throw it so that
+      //  the catch block can convert it to an HTTP error envelope.
+      //  Use res.status() to set the HTTP response status,
+      //  res.json() to send back a JSON response, res.send() to
+      //  send back a non-JSON response.
+    }
+    catch (err) {
+      const mapped = mapResultErrors(err);
+      res.status(mapped.status).json(mapped);
+    }
+  };
+}
 
+function load_courses(app: Express.Application) {
+  return async function (req: Express.Request, res: Express.Response) {
+    try {
+      //code which uses req and res:
+      const course_id = req.params.COURSE_ID;
+      const table = req.query.full;
+      const result = (await app.locals.model.load(course_id, req.body));
+      //check result status
+      if (result.isOk === true) {
+        res.location(course_id);
+      }
+      else {
+        throw result;
+      }
+      //check table query
+      if (table === "true") {
+        //if true get the full table
+        const response = selfResult<G.FullTable>(req, result.val.getFullTable());
+        res.json(response.result);
+      }
+      else
+      //else get the raw table
+      {
+        const response = selfResult<G.FullTable>(req, result.val.getRawTable());
+        res.json(response.result);
+      }
+      //  req.query.X will provide value of query-parameter X.
+      //  req.params.X will provide value of route parameter X.
+      //  req.body will provide request body
+      //  If a Result is in error simply throw it so that
+      //  the catch block can convert it to an HTTP error envelope.
+      //  Use res.status() to set the HTTP response status,
+      //  res.json() to send back a JSON response, res.send() to
+      //  send back a non-JSON response.
+    }
+    catch (err) {
+      const mapped = mapResultErrors(err);
+      res.status(mapped.status).json(mapped);
+    }
+  };
+}
 
+function patch_courses(app: Express.Application) {
+  return async function (req: Express.Request, res: Express.Response) {
+    try {
+      //code which uses req and res:
+      const course_id = req.params.COURSE_ID;
+      const table = req.query.full;
+      const result = (await app.locals.model.patch(course_id, req.body));
+      //check result status
+      if (result.isOk === true) {
+        res.location(course_id);
+      }
+      else {
+        throw result;
+      }
+      //check table query
+      if (table === "true") {
+        //if true get the full table
+        const response = selfResult<G.FullTable>(req, result.val.getFullTable());
+        res.json(response.result);
+      }
+      else
+      //else get the raw table
+      {
+        const response = selfResult<G.FullTable>(req, result.val.getRawTable());
+        res.json(response.result);
+      }
+      //  req.query.X will provide value of query-parameter X.
+      //  req.params.X will provide value of route parameter X.
+      //  req.body will provide request body
+      //  If a Result is in error simply throw it so that
+      //  the catch block can convert it to an HTTP error envelope.
+      //  Use res.status() to set the HTTP response status,
+      //  res.json() to send back a JSON response, res.send() to
+      //  send back a non-JSON response.
+    }
+    catch (err) {
+      const mapped = mapResultErrors(err);
+      res.status(mapped.status).json(mapped);
+    }
+  };
+}
 
 /** Default handler for when there is no route for a particular method
  *  and path.
  */
 function do404(app: Express.Application) {
-  return async function(req: Express.Request, res: Express.Response) {
+  return async function (req: Express.Request, res: Express.Response) {
     const message = `${req.method} not supported for ${req.originalUrl}`;
     const result = {
       status: STATUS.NOT_FOUND,
-      errors: [	{ options: { code: 'NOT_FOUND' }, message, }, ],
+      errors: [{ options: { code: 'NOT_FOUND' }, message, },],
     };
     res.status(404).json(result);
   };
@@ -83,14 +232,14 @@ function do404(app: Express.Application) {
 
 /** Ensures a server error results in nice JSON sent back to client
  *  with details logged on console.
- */ 
+ */
 function doErrors(app: Express.Application) {
-  return async function(err: Error, req: Express.Request, res: Express.Response,
-			next: Express.NextFunction) {
+  return async function (err: Error, req: Express.Request, res: Express.Response,
+    next: Express.NextFunction) {
     const message = err.message ?? err.toString();
     const result = {
       status: STATUS.SERVER_ERROR,
-      errors: [ { options: { code: 'INTERNAL' }, message } ],
+      errors: [{ options: { code: 'INTERNAL' }, message }],
     };
     res.status(STATUS.SERVER_ERROR as number).json(result);
     console.error(result.errors);
@@ -111,18 +260,18 @@ function selfHref(req: Express.Request, id: string = '') {
 }
 
 function selfResult<T>(req: Express.Request, result: T,
-		       status: number = STATUS.OK)
-  : SuccessEnvelope<T>
-{
-  return { isOk: true,
-	   status,
-	   links: { self: { href: selfHref(req), method: req.method } },
-	   result,
-	 };
+  status: number = STATUS.OK)
+  : SuccessEnvelope<T> {
+  return {
+    isOk: true,
+    status,
+    links: { self: { href: selfHref(req), method: req.method } },
+    result,
+  };
 }
 
 
- 
+
 /*************************** Mapping Errors ****************************/
 
 //map from domain errors to HTTP status codes.  If not mentioned in
@@ -140,7 +289,7 @@ const ERROR_MAP: { [code: string]: number } = {
  *  errors, but SERVER_ERROR dominates other statuses.  Returns
  *  BAD_REQUEST if no code found.
  */
-function getHttpStatus(errors: Err[]) : number {
+function getHttpStatus(errors: Err[]): number {
   let status: number = 0;
   for (const err of errors) {
     if (err instanceof Err) {
@@ -157,12 +306,12 @@ function getHttpStatus(errors: Err[]) : number {
  *  object will have a "status" property corresponding to HTTP status
  *  code.
  */
-function mapResultErrors(err: Error|ErrResult) : ErrorEnvelope {
-  const errors = (err instanceof ErrResult) 
+function mapResultErrors(err: Error | ErrResult): ErrorEnvelope {
+  const errors = (err instanceof ErrResult)
     ? err.errors
-    : [ new Err(err.message ?? err.toString()), ];
+    : [new Err(err.message ?? err.toString()),];
   const status = getHttpStatus(errors);
-  if (status === STATUS.SERVER_ERROR)  console.error(errors);
+  if (status === STATUS.SERVER_ERROR) console.error(errors);
   return { isOk: false, status, errors, };
-} 
+}
 
